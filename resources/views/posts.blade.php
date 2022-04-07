64694e92bd73f554aec8016feb535e0c19cab17c
Vinsyahputra <vinthecyborg@gmail.com>
2022-04-08 06:28:47 +0700

    update materi 6-7

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,9 +3,9 @@
 @section('content')
     @foreach ($posts as $post)
         <article>
-            <h1><a href="/posts/{{ $post['slug'] }}">{{ $post['title'] }}</a></h1>
-            <h4>{{ 'Author : '.$post['author'] }}</h4>
-            <p>{{ $post['body'] }}</p>
+            <h1><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h1>
+            <h4>{{ 'Author : '.$post->author }}</h4>
+            <p>{{ $post->excerpt }}</p>
         </article>
     @endforeach
 @endsection
